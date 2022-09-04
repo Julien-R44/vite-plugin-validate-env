e174f910f7089fb9e6af9727faaf1eebe42eb705
Julien <julien@ripouteau.com>
2022-09-04 13:03:23 +0200

    feat: re-assign process.env after parsing + load dedicated env.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,32 @@
+import { cwd } from 'process'
 import { type Plugin, loadEnv } from 'vite'
 import type { Schema } from './contracts'
-import type { ResolvedConfig } from 'vite'
+import type { ConfigEnv, UserConfig } from 'vite'
 
 /**
  * Main function. Will call each validator defined in the schema and throw an error if any of them fails.
  */
-function validateEnv(config: ResolvedConfig, schema: Schema) {
-  const env = loadEnv(config.mode, config.root)
+async function validateEnv(userConfig: UserConfig, envConfig: ConfigEnv, schema?: Schema) {
+  const rootDir = userConfig.root || cwd()
+  const env = loadEnv(envConfig.mode, rootDir)
 
-  for (const [key, validator] of Object.entries(schema)) {
-    validator(key, env[key])
+  if (!schema) {
+    schema = await import(`${rootDir}/env.ts`)
+  }
+
+  for (const [key, validator] of Object.entries(schema!)) {
+    const res = validator(key, env[key])
+    process.env[key] = res
   }
 }
 
 /**
  * Validate environment variables against a schema
  */
-export const ValidateEnv = (schema: Schema): Plugin => {
+export const ValidateEnv = (schema?: Schema): Plugin => {
   return {
-    enforce: 'post',
     name: 'vite-plugin-validate-env',
-    configResolved: (config) => validateEnv(config, schema),
+    config: (config, env) => validateEnv(config, env, schema),
   }
 }
 
