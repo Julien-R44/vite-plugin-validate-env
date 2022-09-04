bac1cd41e71fedfeed4ecf0e8088ff1c214fbd62
Julien <julien@ripouteau.com>
2022-09-04 14:32:01 +0200

    fix: esm config import error

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import { cwd } from 'process'
 import { type Plugin, loadEnv } from 'vite'
+import { createConfigLoader as createLoader } from 'unconfig'
 import type { Schema } from './contracts'
 import type { ConfigEnv, UserConfig } from 'vite'
+
+/**
+ * Load schema defined in `env.ts` file using unconfig
+ */
+async function loadSchemaFromDedicatedFile(rootDir: string) {
+  const loader = createLoader<Schema>({
+    sources: [{ files: 'env.ts' }],
+    cwd: rootDir,
+  })
+
+  const result = await loader.load()
+  return result.config
+}
 
 /**
  * Main function. Will call each validator defined in the schema and throw an error if any of them fails.
@@ -11,7 +25,7 @@ async function validateEnv(userConfig: UserConfig, envConfig: ConfigEnv, schema?
   const env = loadEnv(envConfig.mode, rootDir)
 
   if (!schema) {
-    schema = await import(`${rootDir}/env.ts`)
+    schema = await loadSchemaFromDedicatedFile(rootDir)
   }
 
   for (const [key, validator] of Object.entries(schema!)) {
