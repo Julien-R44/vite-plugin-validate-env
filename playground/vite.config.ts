e7569edd128c6c11702d1049ee10840f2fb59de6
Julien Ripouteau <julien@ripouteau.com>
2025-05-20 01:31:40 +0200

    chore: upgrade playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,40 +3,40 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 // @ts-ignore
-import { z } from 'zod'
+import { z } from 'zod/v4'
 import { defineConfig } from 'vite'
 
 // @ts-ignore
 import { Schema, ValidateEnv } from '../src/index.js'
 
 export default defineConfig({
-  root: __dirname,
+  root: import.meta.dirname,
   plugins: [
-    ValidateEnv({
-      validator: 'builtin',
-      debug: true,
-      schema: {
-        VITE_STRING: Schema.string(),
-        VITE_NUMBER: Schema.number(),
-        VITE_BOOLEAN: Schema.boolean(),
-      },
-    }),
     // ValidateEnv({
-    //   validator: 'zod',
+    //   validator: 'builtin',
     //   debug: true,
     //   schema: {
-    //     VITE_STRING: z.string(),
-    //     VITE_NUMBER: z.preprocess((value) => Number(value), z.number()),
-    //     VITE_BOOLEAN: z.preprocess((value) => value === 'true' || value === '1', z.boolean()),
-
-    //     VITE_OBJECT: z.preprocess(
-    //       (value) => JSON.parse(value as string),
-    //       z.object({
-    //         a: z.string(),
-    //         b: z.number(),
-    //       }),
-    //     ),
+    //     VITE_STRING: Schema.string(),
+    //     VITE_NUMBER: Schema.number(),
+    //     VITE_BOOLEAN: Schema.boolean(),
     //   },
     // }),
+    ValidateEnv({
+      validator: 'standard',
+      debug: false,
+      schema: {
+        VITE_STRING: z.string(),
+        VITE_NUMBER: z.preprocess((value) => Number(value), z.number()),
+        VITE_BOOLEAN: z.preprocess((value) => value === 'true' || value === '1', z.boolean()),
+
+        VITE_OBJECT: z.preprocess(
+          (value) => JSON.parse(value as string),
+          z.object({
+            a: z.string(),
+            b: z.number(),
+          }),
+        ),
+      },
+    }),
   ],
 })
